034a29ece6b36ea40bd42f237b26ecc6f39f274e
blockminerg <85308041+blockminerg@users.noreply.github.com>
2021-06-04 15:20:35 +0300

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The big Tree",
+	id: "blockminerg",
+	author: "blockminer_g",
 	pointsName: "points",
 	discordName: "",
 	discordLink: "",
